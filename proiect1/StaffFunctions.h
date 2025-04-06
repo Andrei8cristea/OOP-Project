bb90c2ede628d8f1b7fb2cd495ba3b7bc531aee9
Andrei8cristea <andrei.cristea1@s.unibuc.ro>
2025-04-06 22:52:17 +0300

    almost finished case 5

---
+++ proiect1/StaffFunctions.h
@@ -0,0 +1,8 @@
+//
+// Created by andrei-cristea on 4/6/25.
+//
+
+#ifndef STAFFFUNCTIONS_H
+#define STAFFFUNCTIONS_H
+
+#endif //STAFFFUNCTIONS_H
---
+++ proiect1/TemplateClasses.h
@@ -0,0 +1,8 @@
+//
+// Created by andrei-cristea on 4/6/25.
+//
+
+#ifndef TEMPLATECLASSES_H
+#define TEMPLATECLASSES_H
+
+#endif //TEMPLATECLASSES_H
